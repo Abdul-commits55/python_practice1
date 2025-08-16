e2154948b80fda3b2b06cb472420f30f3c068cdb
Abdul-commits55 <abdulmoiz1755@gmail.com>
2025-08-16 14:55:55 +0500

    list and type of list
    list and type of list

---
+++ chapter4/list.py
@@ -0,0 +1,4 @@
+friends=["apple", "banana", "cherry",5,35,45,True,False,"mohiz","ahsan"]
+print(friends[0]) 
+friends[0] = "orange" #list is mutable
+print(friends[0])
---
+++ chapter4/list_methods.py
@@ -0,0 +1,21 @@
+friends=["apple", "banana", "cherry",5,35,45,True,False,"mohiz","ahsan"]
+print(friends) 
+friends.append("kiwi")  # Adding a new item to the list
+print(friends)
+
+l1 = [1, 2, 3]
+l1.sort()  # Sorting the list in ascending order
+print(l1)
+
+l1.reverse()  # Reversing the order of the list
+print(l1)
+
+l1.insert(1, 4)  # Inserting an item at a specific index
+print(l1)
+
+l1.pop(l1.pop(3))  # Removing the last item from the list 
+print(l1)
+
+l1.remove(2)  # Removing a specific item from the list
+print(l1)
+
