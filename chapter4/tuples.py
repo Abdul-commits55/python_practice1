902c904dd07c7215af301277996c54875c522843
Abdul-commits55 <abdulmoiz1755@gmail.com>
2025-08-17 18:14:47 +0500

    tuple and method of tuple
    tuple and method of tuple

---
+++ chapter4/tuples.py
@@ -0,0 +1,3 @@
+a=(1,234,43545,252,False,"rohan")
+print(a)
+print(type(a))  # Output: <class 'tuple'>  
---
+++ chapter4/tuples_method.py
@@ -0,0 +1,8 @@
+a=(1,234,43545,252,False,"rohan")
+print(a)
+ 
+no=a.count(252)  # Count occurrences of 252 in the tuple
+print(no)  # Check if 252 is in the tuple
+
+i=a.index(252)  # Find the index of 252 in the tuple
+print(i)  # Output the index of 252
