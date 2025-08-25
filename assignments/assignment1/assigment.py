7980e34719de328515d43c17b3d41be23b690df9
Abdul-commits55 <abdulmoiz1755@gmail.com>
2025-08-25 16:28:53 +0500

    dic and sets
    dic and sets

---
+++ assignments/assignment1/assigment.py
@@ -0,0 +1,24 @@
+# name=input("what is your name:")
+# age=input("how old are you:")
+# fruits=input("enter  3 favourite fruits: ")
+# print=name
+
+                                    #step
+
+# name=input("enter your name:")
+# age=int(input("how old are you:"))
+# fruits=input("enter  3 favourite fruits: ")
+
+# fruits_list=fruits.split(",")
+# print(fruits_list)
+
+                        # step3
+name=input("enter your name:")
+age=int(input("how old are you:"))
+fruits=input("enter  3 favourite fruits: ")
+
+fruits_list=fruits.split(",")
+print(fruits_list)
+print(f"good after noon,{name}")
+print("your name is in uppercase",name.uppercase())
+print("your name length is",len(name))
---
+++ chapter5/set_methods.py
@@ -0,0 +1,9 @@
+s={1,23,45,67,89,"hello"}
+print(s,type(s))
+
+
+s.add(100)
+print(s)
+
+s.remove(23)
+print(s)
